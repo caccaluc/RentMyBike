44aa734d31baff0002e216d2c7c473b97c5e35dd
caccaluc <caccaluc@students.zhaw.ch>
2024-10-28 14:44:03 +0100

    Projekt erstellt

---
+++ src/main/java/ch/zhaw/rentmybike/RentmybikeApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.rentmybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmybikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentmybikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/rentmybike/RentmybikeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.rentmybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentmybikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
