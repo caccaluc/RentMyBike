08e8954aa78de86b67e38aea31c15b653cd77716
caccaluc <caccaluc@students.zhaw.ch>
2024-11-12 17:56:00 +0100

    DTO to create Motorcycle, closes #21

---
+++ src/main/java/ch/zhaw/rentmybike/model/dtos/CreateMotorcycleDTO.java
@@ -0,0 +1,22 @@
+package ch.zhaw.rentmybike.model.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateMotorcycleDTO {
+    
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private String licensePlate;
+    private int value;
+    private int ps;
+    private int km;
+    private String userId;
+}
