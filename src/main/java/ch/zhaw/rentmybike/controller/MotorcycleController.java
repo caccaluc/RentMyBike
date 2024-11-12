1733a15b4987f38f1cdbed3a1ed54da8ca77a970
caccaluc <caccaluc@students.zhaw.ch>
2024-11-12 18:04:07 +0100

    GET-Request for motorcycle, closes #22

--- src/main/java/ch/zhaw/rentmybike/controller/MotorcycleController.java
+++ src/main/java/ch/zhaw/rentmybike/controller/MotorcycleController.java
@@ -1,10 +1,12 @@
 package ch.zhaw.rentmybike.controller;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,6 +42,22 @@ public class MotorcycleController {
         Motorcycle savedMotorcycle = motorcycleRepository.save(motorcycle);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedMotorcycle);
     }
+
+    // Abrufen eines Motorrads basierend auf der ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Motorcycle> getMotorcycleById(@PathVariable String id) {
+        Optional<Motorcycle> motorcycle = motorcycleRepository.findById(id);
+        return motorcycle.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+     // Abrufen aller Motorräder eines bestimmten Nutzers
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Motorcycle>> getMotorcyclesByUserId(@PathVariable String userId) {
+        List<Motorcycle> motorcycles = motorcycleRepository.findByUserId(userId);
+        return ResponseEntity.ok(motorcycles);
+    }
+
+
     
     // Aktualisieren eines bestehenden Motorrads
     @PutMapping("/{id}")
--- src/main/java/ch/zhaw/rentmybike/repository/MotorcycleRepository.java
+++ src/main/java/ch/zhaw/rentmybike/repository/MotorcycleRepository.java
@@ -1,7 +1,12 @@
 package ch.zhaw.rentmybike.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ch.zhaw.rentmybike.model.entities.Motorcycle; 
 
 public interface MotorcycleRepository extends MongoRepository<Motorcycle, String> {
+
+  List<Motorcycle> findByUserId(String userId);
+
 }
