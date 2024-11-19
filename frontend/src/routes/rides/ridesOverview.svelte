ec7ff217f16c0b2a47ae0cd2abbde632b45372e7
caccaluc <caccaluc@students.zhaw.ch>
2024-11-19 17:31:08 +0100

    Webpage for overview, closes #33

---
+++ frontend/src/routes/rides/ridesOverview.svelte
@@ -0,0 +1,69 @@
+<script>
+    import axios from "axios";
+    import { onMount } from "svelte";
+  
+    const api_root = "https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mock.pstmn.io";
+  
+    let rides = [];
+    let ride = {
+        motorcycleId: null,
+        country: null, 
+        city: null,
+        postalCode: null, 
+        street: null, 
+        streetNumber: null,
+        startingTime: null,
+        endingTime: null,
+        price: null,
+      
+    };
+  
+    onMount(() => {
+      getRides();
+    });
+  
+    function getRides() {
+      var config = {
+        method: "get",
+        url: api_root + "/api/rides/status/AVAILABLE",
+        headers: {},
+      };
+  
+      axios(config)
+        .then(function (response) {
+          rides= response.data;
+        })
+        .catch(function (error) {
+          alert("Could not get rides");
+          console.log(error);
+        });
+    }
+</script>
+
+
+<h1>All Jobs</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">MotorradId</th>
+        <th scope="col">Land</th>
+        <th scope="col">Stadt</th>
+        <th scope="col">Start der Vermietung</th>
+        <th scope="col">Ende der Vermietung</th>
+        <th scope="col">Preis</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each rides as ride}
+        <tr>
+          <td>{ride.motorcycleId}</td> <!-- hier will ich nicht die ID anzeigen, sondern Infos vom Motorrad -->
+          <td>{ride.country}</td> <!-- Das Land ist im Objekt pickupAdress verschachtelt, komme ich so trotzdem ran? -->
+          <td>{ride.city}</td>
+          <td>{ride.startingTime}</td>
+          <td>{ride.endingTime}</td>
+          <td>{ride.price}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  
