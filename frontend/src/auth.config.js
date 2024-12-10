28679ae4227f6380f978f03203ac7c6ce98aca9e
caccaluc <caccaluc@students.zhaw.ch>
2024-12-10 17:17:57 +0100

    Implement Login, closes #43

---
+++ frontend/src/auth.config.js
@@ -0,0 +1,5 @@
+const config = {
+    auth0_domain: "dev-qr5nhbnzsf10l3v5.eu.auth0.com",
+    auth0_client_id: "yzVvuITbMyfhOzS6ONbnjUAO1Zqw4J4S"
+};
+export default config;
---
+++ src/test/java/ch/zhaw/rentmybike/controller/RideControllerTest.java
@@ -0,0 +1,5 @@
+package ch.zhaw.rentmybike.controller;
+
+public class RideControllerTest {
+    
+}
